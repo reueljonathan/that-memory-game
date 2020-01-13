05984a8d8a58f58afd06426b70c31701a934b3c4
Reuel Jonathan <reuel.jonathan@gmail.com>
2020-01-13 16:37:50 -0300

    Remove unused files, adds logic to limit size of screen when creating game

--- js/game.js
+++ js/game.js
@@ -1,6 +1,16 @@
 'use strict';
 
-var game = new Phaser.Game('100', '100', Phaser.CANVAS, '', null, false, false);
+const width = ( window.innerWidth < 415 ? window.innerWidth : 415 );
+const height = ( window.innerHeight < 800 ? window.innerHeight : 800 );
+
+const game = new Phaser.Game(
+  width, 
+  height, 
+  Phaser.CANVAS, 
+  'game-container', 
+  null, 
+  false, 
+  false);
 
 function startGame(){
 	game.state.start('boot');
